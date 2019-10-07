5a4eba718935d1164ba341d29a92435788c12af1
RWahl8177 <rwahl005@west-mec.org>
2019-10-07 14:31:17 -0700

    Finished Project
    Finished the project. It is 100% complete.

--- Projects/index.js
+++ Projects/index.js
@@ -4,6 +4,29 @@ var app = express();
 var fs = require('fs');
 var port = 5500;
 const fetch = require("node-fetch");
+const hbs = require('express-handlebars')({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      static(path) {
+        return path;
+      },
+      escapeJSString(str) {
+        if (! str) {
+          return null;
+        }
+        return jsesc(str, {
+          // escape everything to \xFF hex sequences so we don't have to worry about script tags and whatnot
+          escapeEverything: true, 
+          // wrap output with single quotes
+          wrap: true 
+        });
+      }
+    }
+  });
+
+app.engine('hbs', hbs);
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,22 +34,41 @@ app.listen(port, function () {
     console.log('Server listening on localhost:%s', port);
 });
 
+/**
+ * The Main Page
+ */
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    res.render("index", {date: new Date().toDateString()});
 });
 
+/**
+ * The redirect to the proper weather page.
+ */
+app.get("/getweather", (req, res) => {
+    console.log(req.query.lat);
+    res.redirect(`./${req.query.lat}/${req.query.long}/weather`);
+});
+
+/**
+ * The weather data.
+ */
 app.get("*/weather", (req, res) => {
     var pth = req.path.split('/');
     fetch('https://api.weather.gov/points/' + pth[1] + ',' + pth[2]).then(res => res.json()).then(data => {
         var cityName = data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state;
         res.send(`
+            <link rel="stylesheet" href="/weather.css">
             <h1>${cityName}</h1>
             <h3>View the Weather for the following:</h3>
+            <section>
             <ul>
             <li><a href="./today">Today</a></li>
             <li><a href="./hourly">Hourly</a></li>
             <li><a href="./weekly">Weekly</a></li>
             </ul>
+            </section>
+            <p><a href="/">Back</a></p>
         `);
     }).catch(err => {
         res.send(`
@@ -36,6 +78,9 @@ app.get("*/weather", (req, res) => {
     });
 });
 
+/**
+ * Display the today day.
+ */
 app.get("*/weather/today", function (req, res) {
     var pth = req.path.split('/');
     fetch('https://api.weather.gov/points/' + pth[1] + ',' + pth[2]).then(res => res.json()).then(data => {
@@ -55,44 +100,74 @@ app.get("*/weather/today", function (req, res) {
                 html += `</section>`
             }
             html += '</section>'
+            html += `<p id="error"><a href="./">Back</a></p>`;
             res.send(html);
-        }).catch(e => { res.send(e); });
+        }).catch(e => { res.redirect('./') });
 
-    }).catch(e => { res.redirect('./') });
-    // res.sendFile(__dirname + "/today.html");
+    }).catch(e => {res.redirect('./') });
 });
 
-app.get("/weather/hourly", function (req, res) {
-    res.sendFile(__dirname + "/hourly.html");
-});
-
-app.get("/weather/weekly", function (req, res) {
-    res.sendFile(__dirname + "/weekly.html");
-})
-
-app.get("/about", function (req, res) {
-    var html;
-    fetch('https://api.weather.gov/gridpoints/LWX/95,70/forecast').then(e => { return e.json(); }).then(r => {
-        html += `<h2>Today</h2><h3>${new Date().toDateString()}</h3>`;
-        for (var i = 0; i < 3; i++) {
-            html += `<section id="t${i}">`
-            html += `<h4 id="t${i}">${r.properties.periods[i].name}</h4>`;
-            html += `<p class="symbol" id="t${i}symbol"> ${getSymbol(r.properties.periods[i].shortForecast)}</p>`;
-            html += `<p id="t${i}forecast">${r.properties.periods[i].shortForecast}</p>`;
-            html += `<ul><li id="t${i}temp">Temperature: ${r.properties.periods[i].temperature + r.properties.periods[i].temperatureUnit} </li> 
-            <li id="t${i}wind">Wind: ${r.properties.periods[i].windSpeed + " " + r.properties.periods[i].windDirection}</li></ul>`;
-            html += `<h5>Detailed Description:</h5> <p id="t${i}desc">${r.properties.periods[i].detailedForecast}</p>`;
-            html += `</section>`
-        }
-        res.send(`<h1>Example Weather API Usage</h1>
-            <h3>Weather for Washington, D.C.:</h3>
-            <section id="weather">${html}</section>`);
+app.get("*/weather/hourly", function (req, res) {
+    var pth = req.path.split('/');
+    fetch('https://api.weather.gov/points/' + pth[1] + ',' + pth[2]).then(res =>res.json()).then(data => {
+        var cityName = data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state;
+        fetch(data.properties.forecastHourly).then(res =>res.json()).then(data => {
+            let html = `<link rel="stylesheet" href="../../../weather/weathera.css">`;
+            html += `<h1 id='city'>${cityName}</h1>`;
+            html += `<section id="hourly"><h2>Hourly</h2> <table id="hourlyTable"> <tr>`;
+            var periods = data.properties.periods;
+            for(var i = 0; i < 9; i++){
+                html += `<th>${convertTime(new Date(periods[i].startTime))}</th>`;
+            }
+            html += "</tr><tr>";
+            for(var i = 0; i < 9; i++){
+                html += `<td>${getSymbol(periods[i].shortForecast)}</td>`;
+            }
+            html +="</tr><tr>";
+            for(var i = 0; i < 9; i++){
+                html += `<td>${periods[i].temperature + periods[i].temperatureUnit}</td>`;
+            }
+            html +="</tr></table></section>";
+            html += `<p id="error"><a href="./">Back</a></p>`;
+            res.send(html);
+        });
     });
 });
 
-app.get("/weather", function (req, res) {
-    res.send("Please specify a location to get the weather for. <a href='/'>Back</a>");
+app.get("*/weather/weekly", function (req, res) {
+    var pth = req.path.split('/');
+    fetch('https://api.weather.gov/points/' + pth[1] + ',' + pth[2]).then(response => response.json()).then(data => {
+        var cityName = data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state;
+        fetch(data.properties.forecast).then(res =>{return res.json();}).then(data => {
+            let html = `<link rel="stylesheet" href="../../../weather/weathera.css">`;
+            html += `<h1 id='city'>${cityName}</h1>`
+            html += "<section id='weekly'>"
+            html += `<h2>Weekly</h2> <table id="weeklyTable"> <tr>`;
+            var periods = data.properties.periods;
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<th>${periods[i].name}</th>`;
+            }
+            html += "</tr><tr>";
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<td>${getSymbol(periods[i].shortForecast)}</td>`;
+            }
+            html +="</tr><tr>";
+            for(var i = 0; i < periods.length; i++){
+                if(periods[i].name.includes("Night")) continue;
+                html += `<td>${periods[i].temperature + periods[i].temperatureUnit}</td>`;
+            }
+            html +="</tr></table></section>";
+            html += `<p id="error"><a href="./">Back</a></p>`
+            res.send(html);
+        });
+    });
 })
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 function getSymbol(shortForecast) {
     if (shortForecast.toLowerCase().includes("thunderstorm")) return "&#x26C8;&#xFE0E;";
@@ -103,4 +178,10 @@ function getSymbol(shortForecast) {
 
     return "☀";
 
+}
+
+function convertTime(date){
+    var hours = date.getHours();
+    hours = hours > 12 ? (hours - 12) + " PM" : hours + " AM";
+    return hours;
 }
